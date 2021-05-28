8668682a9eaff2711cf48f686e5f31d5b2f8a7b6
Martand <martand.chauhan@infoedge.com>
2021-05-28 11:58:35 +0530

    beta changelist: adds run scripts

---
+++ start.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function execute() {
+  try {
+    await exec("npm install yarn");
+    await exec("npm install");
+    await exec("npx webpack --config webpack.config.js --no-watch");
+    await exec("cp src/index.html dist/index.html")
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function run() {
+  await execute();
+  app.use(express.static(__dirname + "dist")); //Serves resources from public folder
+  app.listen(5000);
+}
+
+run();
