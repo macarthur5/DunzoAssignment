02915381ddfa73a0deb3b72097a7b4b8ef7fb9dc
Martand <martand.chauhan@infoedge.com>
2021-05-28 12:44:20 +0530

    beta changelist: adds start.js

--- start.js
+++ start.js
@@ -1,16 +1,22 @@
-const express = require('express');
-const app = express();
-
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 ////////
 async function execute() {
   try {
-    await exec("npm install yarn");
-    await exec("npm install");
+    console.log("installing yarn");
+    await exec("npm install yarn --save-dev");
+
+    console.log("installing packages");
+    await exec("yarn install");
+
+    console.log("building project");
     await exec("npx webpack --config webpack.config.js --no-watch");
+
+    console.log("generating build files");
     await exec("cp src/index.html dist/index.html")
+
+    console.log("open dist/index.html in the browser to run the app.")
   } catch (err) {
     console.log(err);
   }
@@ -19,7 +25,7 @@ async function execute() {
 async function run() {
   await execute();
   app.use(express.static(__dirname + "dist")); //Serves resources from public folder
-  app.listen(5000);
+  app.listen(5500);
 }
 
 run();
